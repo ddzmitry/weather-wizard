613cbfadec3bb0b5c59e2df83ed1f5e6ec826606
Dzmitry Dubarau <ddzmitry@yahoo.com>
2017-01-25 14:28:12 -0500

    Firebase Auth

---
+++ script.js
@@ -0,0 +1,267 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDis8TTOcaDju9g8zqWrlNIei5g5hQiyNc",
+    authDomain: "authlearning-31116.firebaseapp.com",
+    databaseURL: "https://authlearning-31116.firebaseio.com",
+    storageBucket: "authlearning-31116.appspot.com",
+    messagingSenderId: "927237143466"
+  };
+firebase.initializeApp(config);
+var database = firebase.database();
+var user = database.ref('/user');
+var newUser = database.ref('/newuser');
+var email;
+var pass;
+
+user.on('child_added', function(snap){
+          email =  snap.val().email
+          pass =   snap.val().pass
+
+          console.log(email)
+          console.log(snap.val())
+
+          $('#txtEmail').val(email)
+          $('#txtPassword').val(pass)
+});
+
+
+//gET elEMENTS
+const txtEmail = document.getElementById('txtEmail');
+const txtPassword = document.getElementById('txtPassword');
+const btnLogin = document.getElementById('btnLogin');
+const btnSignUp = document.getElementById('btnSignUp');
+const btnLogout = document.getElementById('btnLogout');
+//add login
+
+btnLogin.addEventListener('click', e => {
+  //Get emeil and pass
+  const email = txtEmail.value;
+  const pass = txtPassword.value;
+  const auth = firebase.auth();
+
+  // Sign in
+  user.push({
+
+          email : email, 
+          pass  : pass,
+          uid: ''
+
+  })
+
+  const promise = auth.signInWithEmailAndPassword(email, pass);
+
+  promise.catch(e => console.log(e.message));
+
+console.log('loggedin')
+
+});
+// sign up 
+
+btnSignUp.addEventListener('click', e => {
+
+  const email = txtEmail.value;
+  const pass = txtPassword.value;
+  const auth = firebase.auth();
+  // Sign up
+  $('.info').show()
+  newUser.push({
+          email : email,
+          pass: pass,
+          status: 'loggedin',
+          zpi: '',
+          uid: ''
+    } );
+  $('#weathers').empty();
+
+  const promise = auth.createUserWithEmailAndPassword(email, pass);
+
+  promise.catch(e => console.log(e.message));
+});
+
+btnLogout.addEventListener('click', e => {
+    newUser.update({
+          status: 'loggedOUT'
+    } );
+
+  firebase.auth().signOut();
+})
+
+
+//add real time listener
+
+firebase.auth().onAuthStateChanged(firebaseUser => {
+  if (firebaseUser) {
+    $('.info').show()
+    console.log(firebaseUser);
+    console.log(firebaseUser.uid);
+
+
+    btnLogout.classList.remove('hide');
+  } else {
+    
+    $('.info').hide()
+    console.log("not logged in");
+    btnLogout.classList.add('hide');
+  }
+
+});
+var pos;
+var weatherReport;
+var lowtemp;
+var eventsNames = [];
+$('.info').hide()
+$('#lookInfo').on('click', function() {
+
+  zip = $('.search').val().trim()
+  var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(`${zip}`);
+  if (isValidZip === true) {
+// user.({zipcode: zip})
+$('#weathers').empty()
+    console.log(zip)
+    findLocation(zip)
+  } else {
+
+    console.log('bad zip')
+  }
+
+
+})
+
+function findLocation(zip) {
+  $.ajax({
+      url: `https://maps.googleapis.com/maps/api/geocode/json?address=${zip}&key=AIzaSyATHPPagioRnJR7xhCvEYBT2VVFUkE5ajY`,
+    })
+    .done(function(data) {
+      console.log(data)
+
+
+      lat = data.results[0].geometry.bounds.northeast.lat,
+        lng = data.results[0].geometry.bounds.northeast.lng
+      checkWeather(lat, lng)
+
+
+      // console.log("success");
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+}
+
+function checkWeather(lat, lng) {
+  var url = `http://api.wunderground.com/api/3f4f6b8d728af2d4/forecast10day/q/${lat},${lng}.json`
+  $.ajax({
+      url: url, // here we get our weather from at and long from first api call
+
+    })
+    .done(function(data) {
+
+
+
+      // console.log(data.forecast.simpleforecast.forecastday.length);
+
+      var arrWeathers = data.forecast.simpleforecast.forecastday;
+
+      for (i in arrWeathers) {
+
+
+        // console.log(arrWeathers[i]);
+
+        // console.log(arrWeathers[i].date.epoch);
+        // console.log(arrWeathers[i].date.monthname_short);
+        // console.log(arrWeathers[i].date.day);
+        // console.log(arrWeathers[i].conditions);
+        // console.log(arrWeathers[i].low.fahrenheit);
+        // console.log(arrWeathers[i].high.fahrenheit); 
+        // console.log(arrWeathers[i].high.icon_url);
+        if (i <= 4) {
+          date = `${arrWeathers[i].date.monthname_short } ${arrWeathers[i].date.day}`
+            // console.log(date)
+
+
+          $("#weathers").append(`<div  class="weathertag Day${i}" data-day = "${date}" ">
+                                    <p>${arrWeathers[i].date.monthname_short} ${arrWeathers[i].date.day} </p>
+                                    <p>Condition ${arrWeathers[i].conditions}</p>
+                                    <p>Low temperature ${arrWeathers[i].low.fahrenheit}</p>
+                                    <p>High temperature ${arrWeathers[i].high.fahrenheit}</p>                              
+                                    <img class="imgWeather${i}" src="${arrWeathers[i].icon_url}"
+                                    
+                                    </div>
+                                    <button class="meetupBtn"  data-day = "${date}" > Meetup </button> 
+                                    `)
+        }
+      }
+
+      $('.meetupBtn').on('dblclick', function(event) {
+        event.preventDefault();
+        // console.log($(this).data().day)
+
+        day = $(this).data().day
+        _this = $(this)
+
+        $.ajax({
+            url: `https://api.meetup.com/find/events?photo-host=public&sig_id=211596974&lon=${lng}&lat=${lat}&sig=38c2cf24f276b84aafa5431fca7ea8a893f594e4`,
+            dataType: 'jsonp'
+
+          })
+          .done(function(data) {
+            // console.log(data)
+            // console.log(data.data)
+            // console.log("success");
+            var events = data.data;
+            for (i in events) {
+
+              if (events[i].hasOwnProperty('venue')) {
+
+                str = (moment(events[i].time)._d).toString()
+                str = str.slice(4, 10).trim()
+                  // console.log(str)
+                if (str == day) {
+
+
+                  console.log(events[i].link)
+                  console.log(events[i].name)
+                    // console.log(events[i].venue.address_1)
+                    // console.log(events[i].venue.name)
+                    // console.log("---------------------------------")
+
+                  _this.append(`<div class='meetupEvent'> 
+                                          <p>Name: ${events[i].name}</p>
+                                          <p>Adress: ${events[i].venue.address_1}</p>
+                                          <p>Venue Name: ${events[i].venue.name} </p>
+                                          <a href="${events[i].link}" target="_blank">Link on Event</a>
+                                          <p>___________________________</p>
+                              <div>`)
+
+
+                }
+
+
+              }
+            }
+          })
+
+        console.log(eventsNames)
+
+
+      });
+
+      $('.meetupBtn').on('click', function() {
+
+        $(this).html('Meetup')
+
+      })
+
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+
+    });
+}
+
+console.log("ready!");
